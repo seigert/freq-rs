f15db94b468ce5980ff6da5ab3209fe13ac6cfbd
Alexey Shuksto <alexey@shuksto.name>
2020-05-18 20:40:00 +0300

    use custom hash map

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::{
+    cmp::Ordering,
     fs::File,
     io::{BufWriter, Read, Write},
 };
 
 use clap::Clap;
-use fnv::FnvHashMap;
 use memmap::*;
 
-const H: u32 = 2166136261;
-const P: u32 = 0x1000193;
+const H: usize = 2166136261;
+const P: usize = 0x1000193;
 
 /// Counts number of unique `[a-zA-Z]+` words in input.
 #[derive(Clap, Debug)]
@@ -20,46 +20,36 @@ struct Opts {
     output: Option<String>,
 }
 
-struct FreqDict {
-    hashes: FnvHashMap<u32, FreqEntry>,
-    collisions: FnvHashMap<Box<[u8]>, u32>,
-}
-
-enum FreqEntry {
-    Collision,
-    Value(Box<[u8]>, u32),
-}
-
 fn main() {
     let opts: Opts = Opts::parse();
     let input = open_mmap(&opts);
 
     let mut word = Vec::with_capacity(256);
     let mut hash = H;
-    let mut dict = FreqDict::new();
+    let mut dict = FrequencyHashMap::new();
 
     for &byte in input.iter() {
         if b'a' <= byte && byte <= b'z' {
             word.push(byte);
-            hash = (hash ^ byte as u32) * P;
+            hash = (hash ^ byte as usize) * P;
             continue;
         } else if b'A' <= byte && byte <= b'Z' {
             let byte = byte ^ 0x20;
             word.push(byte);
-            hash = (hash ^ byte as u32) * P;
+            hash = (hash ^ byte as usize) * P;
             continue;
         } else if !word.is_empty() {
-            dict.add_word(&word, hash);
+            *dict.put_mut(hash, &word) += 1;
             word.clear();
             hash = H;
         }
     }
     if !word.is_empty() {
-        dict.add_word(&word, hash);
+        *dict.put_mut(hash, &word) += 1;
     }
 
     let mut output = create_output(&opts);
-    for (count, word) in dict.get_freq() {
+    for (count, word) in dict.into_iter() {
         writeln!(&mut output, "{} {}", count, word).unwrap_or_else(|e| {
             let output = opts.output.as_ref().map_or("-", |s| s.as_str());
             panic!("Unable to write results in '{}': {}", output, e)
@@ -67,59 +57,142 @@ fn main() {
     }
 }
 
-impl FreqDict {
-    fn new() -> Self {
-        FreqDict {
-            hashes: FnvHashMap::default(),
-            collisions: FnvHashMap::default(),
+struct FrequencyHashMap {
+    buckets: Vec<Option<FrequencyHashEntry>>,
+    capacity: usize,
+    length: usize,
+    mask: usize,
+    max: usize,
+}
+
+#[derive(Clone, Debug)]
+struct FrequencyHashEntry {
+    key: Box<[u8]>,
+    value: usize,
+    hash: usize,
+}
+
+impl FrequencyHashMap {
+    const INITIAL: usize = 128;
+    const LOAD_FACTOR: f32 = 0.9;
+
+    fn new() -> FrequencyHashMap {
+        FrequencyHashMap {
+            buckets: vec![None; Self::INITIAL],
+            capacity: Self::INITIAL,
+            length: 0,
+            mask: Self::INITIAL - 1,
+            max: (Self::LOAD_FACTOR * Self::INITIAL as f32) as usize,
         }
     }
 
-    #[inline]
-    fn add_word(&mut self, word: &[u8], hash: u32) {
-        if let Some(entry) = self.hashes.get_mut(&hash) {
-            match entry {
-                FreqEntry::Collision => {
-                    if let Some(counter) = self.collisions.get_mut(word) {
-                        *counter += 1;
+    fn put_mut(&mut self, hash: usize, word: &[u8]) -> &mut usize {
+        self.ensure_capacity();
+
+        let mut index = hash & self.mask;
+        loop {
+            match &self.buckets[index] {
+                Some(entry) => {
+                    if hash != entry.hash || word.as_ref() != entry.key.as_ref() {
+                        index = (index + 1) & self.mask
                     } else {
-                        self.collisions.insert(word.into(), 1);
+                        break;
                     }
                 }
-                FreqEntry::Value(ref value, ref mut counter) =>
-                    if word == value.as_ref() {
-                        *counter += 1;
-                    } else {
-                        if let FreqEntry::Value(value, counter) = std::mem::replace(entry, FreqEntry::Collision) {
-                            self.collisions.insert(value, counter);
-                            self.collisions.insert(word.into(), 1);
-                        }
-                    }
+                None => {
+                    self.buckets[index] = Some(FrequencyHashEntry {
+                        key: word.into(),
+                        value: 0,
+                        hash,
+                    });
+                    self.length += 1;
+                    break;
+                }
             }
-        } else {
-            self.hashes.insert(hash, FreqEntry::Value(word.into(), 1));
         }
+
+        &mut self.buckets[index].as_mut().unwrap().value
     }
 
-    fn get_freq(&self) -> Vec<(u32, &str)> {
-        let hashed = self.hashes.values()
-            .filter_map(|e| match e {
-                FreqEntry::Collision => None,
-                FreqEntry::Value(value, counter) => {
-                    let key = std::str::from_utf8(value).unwrap();
-                    Some((*counter, key))
-                }
-            });
-        let collided = self.collisions.iter().map(|(value, counter)| {
-            let key = std::str::from_utf8(value).unwrap();
-            (*counter, key)
-        });
+    fn ensure_capacity(&mut self) {
+        if self.length > self.max {
+            assert!(self.length < 2 * self.capacity);
 
-        let mut freq = Iterator::chain(hashed, collided).collect::<Vec<_>>();
-        freq.sort_unstable_by(|(c1, w1), (c2, w2)| {
-            Ord::cmp(c1, c2).reverse().then_with(|| Ord::cmp(w1, w2))
-        });
-        freq
+            self.capacity = 2 * self.capacity;
+            self.mask = self.capacity - 1;
+            self.max = (Self::LOAD_FACTOR * self.capacity as f32) as usize;
+
+            let new_buckets = vec![None; self.capacity];
+            let old_buckets = std::mem::replace(&mut self.buckets, new_buckets);
+            for bucket in old_buckets {
+                if let Some(entry) = bucket {
+                    let mut index = entry.hash & self.mask;
+                    while let Some(_) = self.buckets[index] {
+                        index = (index + 1) & self.mask;
+                    }
+
+                    self.buckets[index] = Some(entry);
+                }
+            }
+        }
+    }
+}
+
+struct FrequencyIntoIter {
+    iter: std::vec::IntoIter<Option<FrequencyHashEntry>>
+}
+
+impl Iterator for FrequencyIntoIter {
+    type Item = (usize, String);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(opt) = self.iter.next() {
+            if let Some(entry) = opt {
+                let key = std::str::from_utf8(entry.key.as_ref()).unwrap().to_owned();
+                return Some((entry.value, key));
+            }
+        }
+        None
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (_, upper) = self.iter.size_hint();
+        (0, upper)
+    }
+}
+
+impl IntoIterator for FrequencyHashMap {
+    type Item = (usize, String);
+    type IntoIter = FrequencyIntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let mut buckets = self.buckets;
+        buckets.sort_unstable();
+
+        FrequencyIntoIter {
+            iter: buckets.into_iter()
+        }
+    }
+}
+
+impl Ord for FrequencyHashEntry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        Ord::cmp(&other.value, &self.value)
+            .then_with(|| Ord::cmp(self.key.as_ref(), other.key.as_ref()))
+    }
+}
+
+impl PartialOrd for FrequencyHashEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(Ord::cmp(&self, &other))
+    }
+}
+
+impl Eq for FrequencyHashEntry {}
+
+impl PartialEq for FrequencyHashEntry {
+    fn eq(&self, other: &Self) -> bool {
+        Ord::cmp(&self, &other) == Ordering::Equal
     }
 }
 
