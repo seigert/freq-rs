ed757160d0fd16819289287fb849e5c06fedc447
Alexey Shuksto <alexey@shuksto.name>
2020-05-18 18:03:54 +0300

    use memory maps for input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use clap::Clap;
-use fnv::FnvHashMap;
 use std::{
     fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{BufWriter, Read, Write},
 };
+
+use clap::Clap;
+use fnv::FnvHashMap;
+use memmap::*;
+
+const H: u32 = 2166136261;
+const P: u32 = 0x1000193;
 
 /// Counts number of unique `[a-zA-Z]+` words in input.
 #[derive(Clap, Debug)]
@@ -16,42 +21,44 @@ struct Opts {
 }
 
 struct FreqDict {
-    dict: FnvHashMap<Box<[u8]>, u32>,
+    hashes: FnvHashMap<u32, FreqEntry>,
+    collisions: FnvHashMap<Box<[u8]>, u32>,
+}
+
+enum FreqEntry {
+    Collision,
+    Value(Box<[u8]>, u32),
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    let mut input = open_input(&opts);
-    let mut output = create_output(&opts);
+    let input = open_mmap(&opts);
 
-    let mut word = Vec::with_capacity(16);
+    let mut word = Vec::with_capacity(256);
+    let mut hash = H;
     let mut dict = FreqDict::new();
 
-    let mut buffer = [0u8; 16 * 1024];
-    loop {
-        let read_count = input
-            .read(&mut buffer)
-            .unwrap_or_else(|e| panic!("Unable to read bytes from '{}': {}", opts.input, e));
-
-        if read_count == 0 {
-            break;
-        }
-
-        for &byte in buffer.iter().take(read_count) {
-            if b'a' <= byte && byte <= b'z' {
-                word.push(byte);
-                continue;
-            } else if b'A' <= byte && byte <= b'Z' {
-                word.push(byte ^ 0x20);
-                continue;
-            }
-
-            dict.add_word(&word);
+    for &byte in input.iter() {
+        if b'a' <= byte && byte <= b'z' {
+            word.push(byte);
+            hash = (hash ^ byte as u32) * P;
+            continue;
+        } else if b'A' <= byte && byte <= b'Z' {
+            let byte = byte ^ 0x20;
+            word.push(byte);
+            hash = (hash ^ byte as u32) * P;
+            continue;
+        } else if !word.is_empty() {
+            dict.add_word(&word, hash);
             word.clear();
+            hash = H;
         }
     }
-    dict.add_word(&word);
+    if !word.is_empty() {
+        dict.add_word(&word, hash);
+    }
 
+    let mut output = create_output(&opts);
     for (count, word) in dict.get_freq() {
         writeln!(&mut output, "{} {}", count, word).unwrap_or_else(|e| {
             let output = opts.output.as_ref().map_or("-", |s| s.as_str());
@@ -63,27 +70,52 @@ fn main() {
 impl FreqDict {
     fn new() -> Self {
         FreqDict {
-            dict: FnvHashMap::default(),
+            hashes: FnvHashMap::default(),
+            collisions: FnvHashMap::default(),
         }
     }
 
-    fn add_word(&mut self, word: &[u8]) {
-        if !word.is_empty() {
-            if let Some(counter) = self.dict.get_mut(word) {
-                *counter += 1;
-            } else {
-                self.dict.insert(word.into(), 1);
+    #[inline]
+    fn add_word(&mut self, word: &[u8], hash: u32) {
+        if let Some(entry) = self.hashes.get_mut(&hash) {
+            match entry {
+                FreqEntry::Collision => {
+                    if let Some(counter) = self.collisions.get_mut(word) {
+                        *counter += 1;
+                    } else {
+                        self.collisions.insert(word.into(), 1);
+                    }
+                }
+                FreqEntry::Value(ref value, ref mut counter) =>
+                    if word == value.as_ref() {
+                        *counter += 1;
+                    } else {
+                        if let FreqEntry::Value(value, counter) = std::mem::replace(entry, FreqEntry::Collision) {
+                            self.collisions.insert(value, counter);
+                            self.collisions.insert(word.into(), 1);
+                        }
+                    }
             }
+        } else {
+            self.hashes.insert(hash, FreqEntry::Value(word.into(), 1));
         }
     }
 
     fn get_freq(&self) -> Vec<(u32, &str)> {
-        let mut freq = self.dict.iter()
-            .map(|(w, c)| {
-                let key = std::str::from_utf8(w).unwrap();
-                (*c, key)
-            })
-            .collect::<Vec<_>>();
+        let hashed = self.hashes.values()
+            .filter_map(|e| match e {
+                FreqEntry::Collision => None,
+                FreqEntry::Value(value, counter) => {
+                    let key = std::str::from_utf8(value).unwrap();
+                    Some((*counter, key))
+                }
+            });
+        let collided = self.collisions.iter().map(|(value, counter)| {
+            let key = std::str::from_utf8(value).unwrap();
+            (*counter, key)
+        });
+
+        let mut freq = Iterator::chain(hashed, collided).collect::<Vec<_>>();
         freq.sort_unstable_by(|(c1, w1), (c2, w2)| {
             Ord::cmp(c1, c2).reverse().then_with(|| Ord::cmp(w1, w2))
         });
@@ -91,13 +123,29 @@ impl FreqDict {
     }
 }
 
-fn open_input(opts: &Opts) -> Box<dyn BufRead> {
+fn open_mmap(opts: &Opts) -> Mmap {
     match opts.input.as_str() {
-        "-" => Box::new(BufReader::new(std::io::stdin())),
+        "-" => {
+            let mut buffer = vec![];
+            std::io::stdin()
+                .read_to_end(&mut buffer)
+                .unwrap_or_else(|e| panic!("Unable to read STDIN: {}", e));
+            let mut mmap = MmapOptions::new()
+                .len(buffer.len())
+                .map_anon()
+                .unwrap_or_else(|e| panic!("Unable to read STDIN: {}", e));
+            mmap.copy_from_slice(&buffer);
+            mmap.make_read_only()
+                .unwrap_or_else(|e| panic!("Unable to read STDIN: {}", e))
+        }
         fnm => {
             let file = File::open(fnm)
                 .unwrap_or_else(|e| panic!("Unable to open '{}' for reading: {}", fnm, e));
-            Box::new(BufReader::new(file))
+            unsafe {
+                MmapOptions::new()
+                    .map(&file)
+                    .unwrap_or_else(|e| panic!("Unable to read '{}' in memory: {}", fnm, e))
+            }
         }
     }
 }
